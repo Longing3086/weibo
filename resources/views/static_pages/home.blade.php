7a05ea6ed9f61d223c0a618992b505368b9fdf33
deng <812084127@qq.com>
2020-02-11 05:55:36 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,15 +1,30 @@
 @extends('layout.default')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在看到的就是一个微博系统啦
-    </p>
-    <p>
-      一切将从这里开始
-    </p>
-    <p>
-      <a href="{{route('signUp')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
-    </p>
-  </div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info',['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在看到的就是一个微博系统啦
+      </p>
+      <p>
+        一切将从这里开始
+      </p>
+      <p>
+        <a href="{{route('signUp')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
+      </p>
+    </div>
+  @endif
 @stop
